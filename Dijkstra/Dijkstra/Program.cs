466493e7f3f0e014449ac588ce580e771faf50b5
Gahyun-Lee <98022068+Gahyun-Lee@users.noreply.github.com>
2024-01-11 09:43:25 +0900

    Sourcetree test

--- Dijkstra/Dijkstra/Program.cs
+++ Dijkstra/Dijkstra/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Dijkstra
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============Dijkstra===============");
+        }
+    }
+}
